61aa1d1f0f1fb1d5d035940cac70dcd5c1d14bd7
Gene Pressinger <genepressinger@yahoo.com>
2016-04-27 13:40:17 -0400

    Added welcome and directions label

--- StudyBuddy/StudyBuddy/LoadInForm.Designer.cs
+++ StudyBuddy/StudyBuddy/LoadInForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StudyBuddy
 {
-    partial class Form1
+    partial class LoadInForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.DirectionsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.AutoSize = true;
+            this.WelcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeLabel.Location = new System.Drawing.Point(50, 9);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(185, 16);
+            this.WelcomeLabel.TabIndex = 0;
+            this.WelcomeLabel.Text = "Welcome to Study Buddy!";
+            this.WelcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DirectionsLabel
+            // 
+            this.DirectionsLabel.AutoSize = true;
+            this.DirectionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirectionsLabel.Location = new System.Drawing.Point(39, 25);
+            this.DirectionsLabel.Name = "DirectionsLabel";
+            this.DirectionsLabel.Size = new System.Drawing.Size(207, 32);
+            this.DirectionsLabel.TabIndex = 1;
+            this.DirectionsLabel.Text = "Select one of the options below to\nstart studying!";
+            this.DirectionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LoadInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.DirectionsLabel);
+            this.Controls.Add(this.WelcomeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Study Buddy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label WelcomeLabel;
+        private System.Windows.Forms.Label DirectionsLabel;
     }
 }
-
